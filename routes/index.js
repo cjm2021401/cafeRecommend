1b83ce742efa1ee1f9f5594d3e477d1ce90ec0c1
dckat <kdc9619@naver.com>
2021-05-12 20:50:07 +0900

    지도 띄우기 구현

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var {OAuth2Client} = require('google-auth-library');
-var CLIENT_ID = "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com"
-var client= new OAuth2Client(CLIENT_ID);
-var session =require('express-session');
-var FileStore=require('session-file-store')(session);
-router.use(session({
-    secret: '209',  // 암호화
+var { OAuth2Client } = require("google-auth-library");
+var CLIENT_ID =
+  "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com";
+var client = new OAuth2Client(CLIENT_ID);
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+router.use(
+  session({
+    secret: "209", // 암호화
     resave: false,
     saveUninitialized: true,
-    store: new FileStore()
-}))
+    store: new FileStore(),
+  })
+);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { d: "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com" });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    d: "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com",
+  });
 });
-router.get('/index', function(req, res, next) {
-  res.render('index', { d: "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com" });
+router.get("/index", function (req, res, next) {
+  res.render("index", {
+    d: "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com",
+  });
 });
 
-router.post('/index', (req, res) => {
-  let token=req.body.token;
+router.post("/index", (req, res) => {
+  let token = req.body.token;
   async function verify() {
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
     });
   }
   verify()
-      .then(()=>{
-        res.cookie('session-token', token);
-        res.send('success')
-      })
-      .catch(console.error);
+    .then(() => {
+      res.cookie("session-token", token);
+      res.send("success");
+    })
+    .catch(console.error);
 });
 
-router.get('/login', checkAuthenticated, (req,res )=>{
-  let user=req.user;
-  req.session.user=user;
-  res.render('login', {user:req.session.user})
+router.get("/login", checkAuthenticated, (req, res) => {
+  let user = req.user;
+  req.session.user = user;
+  res.render("login", { user: req.session.user });
 });
 
-router.post('/login' ,(req, res)=>{
-
-    console.log(req.body.nickname);
-    console.log(req.body.age);
-    console.log(req.body.gender);
-    return res.render('login', {user:req.session.user})
+router.post("/login", (req, res) => {
+  console.log(req.body.nickname);
+  console.log(req.body.age);
+  console.log(req.body.gender);
+  return res.render("login", { user: req.session.user });
 });
 module.exports = router;
 
-
-
-function checkAuthenticated(req, res, next){
-
-  let token = req.cookies['session-token'];
+function checkAuthenticated(req, res, next) {
+  let token = req.cookies["session-token"];
 
   let user = {};
   async function verify() {
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
     });
     const payload = ticket.getPayload();
     user.name = payload.name;
@@ -70,12 +73,15 @@ function checkAuthenticated(req, res, next){
     console.log(user.name);
   }
   verify()
-      .then(()=>{
-        req.user = user;
-        next();
-      })
-      .catch(err=>{
-        res.redirect('/index')
-      })
-
+    .then(() => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      res.redirect("/index");
+    });
 }
+
+router.get("/map", (req, res) => {
+  res.render("map");
+});
