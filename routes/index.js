4f06d11709bab77f76c995241e0f4afa88f2daab
mayf <cjm2021401@khu.ac.kr>
2021-05-17 03:58:15 +0900

    FEAT : MYSQL DB와 연동하여 로그인
    -

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var bodyParser=require('body-parser');
 var { OAuth2Client } = require("google-auth-library");
 var CLIENT_ID =
   "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com";
 var client = new OAuth2Client(CLIENT_ID);
+var mysql=require('mysql')
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+router.use(bodyParser.urlencoded({extended:false})); //url인코딩 x
+router.use(bodyParser.json());  //json방식으로 파
 router.use(
   session({
     secret: "209", // 암호화
@@ -14,7 +18,13 @@ router.use(
     store: new FileStore(),
   })
 );
-
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'g79465',
+  database : 'caferecommend'
+});
+connection.connect();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
@@ -44,16 +54,60 @@ router.post("/index", (req, res) => {
 });
 
 router.get("/login", checkAuthenticated, (req, res) => {
-  let user = req.user;
-  req.session.user = user;
-  res.render("login", { user: req.session.user });
+  var sql = 'SELECT * FROM USER WHERE EMAIL=?';
+  var parameter=[req.session.user.email];
+  connection.query(sql,parameter, function(err, row){
+    if(err){
+      console.log(err);
+    }
+    else {
+      if (row.length > 0) {
+        console.log("이미 가입이 되어있는 아이디")
+        req.session.user.nickname = row[0].NICKNAME;
+        req.session.user.age = row[0].AGE;
+        req.session.user.gender = row[0].GENDER;
+        return res.render('map', {user : req.session.user})
+      }
+      else{
+        return res.render("login", { user: req.session.user, message:'none' });
+      }
+    }
+  });
 });
 
 router.post("/login", (req, res) => {
   console.log(req.body.nickname);
   console.log(req.body.age);
   console.log(req.body.gender);
-  return res.render("login", { user: req.session.user });
+  var sql =' SELECT * FROM USER WHERE NICKNAME=?';
+  var parameter=[req.body.nickname];
+  connection.query(sql, parameter, function(err,row){
+      if(err){
+        console.log(err);
+      }
+      if(row.length>0){
+        console.log('동일 닉네임있음');
+
+        return res.render("login", {user : req.session.user, message: 'same nickname'});
+      }
+      else{
+        req.session.user.nickname=req.body.nickname;
+        req.session.user.age=req.body.age;
+        req.session.user.gender=req.body.gender;
+        var sql='INSERT INTO USER(EMAIL, NAME, NICKNAME, AGE, GENDER) VALUES(?,?,?,?,?)';
+        var parameter=[req.session.user.email, req.session.user.name, req.session.user.nickname, req.session.user.age, req.session.user.gender];
+        connection.query(sql, parameter, function(err, row){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log("새로운 user데이터 입력");
+          }
+        });
+        return res.render("map", { user : req.session.user });
+      }
+  });
+
 });
 module.exports = router;
 
@@ -69,12 +123,11 @@ function checkAuthenticated(req, res, next) {
     const payload = ticket.getPayload();
     user.name = payload.name;
     user.email = payload.email;
-    user.picture = payload.picture;
-    console.log(user.name);
   }
   verify()
     .then(() => {
-      req.user = user;
+      req.session.user.name = user.name;
+      req.session.user.email=user.email;
       next();
     })
     .catch((err) => {
@@ -83,5 +136,12 @@ function checkAuthenticated(req, res, next) {
 }
 
 router.get("/map", (req, res) => {
+  console.log(req.session.user);
   res.render("map");
+});
+
+
+router.get('/logout',function(req,res){
+  req.session.destroy();  //세션비우기
+  res.redirect('/');
 });
