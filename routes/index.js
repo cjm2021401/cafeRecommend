2f004d1dcc537b29fcdc2727c2e7b38f9e78179b
dckat <kdc9619@naver.com>
2021-05-17 15:22:49 +0900

    카페 리스트 API 호출

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var request = require("request");
+
 var { OAuth2Client } = require("google-auth-library");
+var querystring = require("querystring");
+
 var CLIENT_ID =
   "94679084723-s5f0686p2porp9mkakrp1p89a48n24nj.apps.googleusercontent.com";
 var client = new OAuth2Client(CLIENT_ID);
@@ -55,6 +59,37 @@ router.post("/login", (req, res) => {
   console.log(req.body.gender);
   return res.render("login", { user: req.session.user });
 });
+
+router.get("/cafe", (req, res) => {
+  let code = "CE7";
+  let encodedStr = querystring.escape(code);
+
+  let kakaoOptions = {
+    uri: `https://dapi.kakao.com/v2/local/search/category.json?category_group_code=${encodedStr}`,
+    method: "GET",
+    headers: {
+      Authorization: "KakaoAK 2f3999076db5d32db975ab9862a64480",
+    },
+    encoding: "utf-8",
+  };
+  request(kakaoOptions, callback);
+
+  function callback(error, res, body) {
+    console.log(body);
+    let kakaoPlaces = JSON.parse(body);
+
+    for (document of kakaoPlaces.documents) {
+      console.log(document.id);
+      console.log(document.place_name);
+    }
+  }
+  return;
+});
+
+router.get("/map", (req, res) => {
+  res.render("map");
+});
+
 module.exports = router;
 
 function checkAuthenticated(req, res, next) {
@@ -81,7 +116,3 @@ function checkAuthenticated(req, res, next) {
       res.redirect("/index");
     });
 }
-
-router.get("/map", (req, res) => {
-  res.render("map");
-});
